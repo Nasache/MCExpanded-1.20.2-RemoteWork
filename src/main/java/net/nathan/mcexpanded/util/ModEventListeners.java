aa3a7abd83677f6dc3f3f75ad22d7a5aae406e72
Nasache <155038784+Nasache@users.noreply.github.com>
2024-02-02 19:06:49 -0500

    Initial commit

--- src/main/java/net/nathan/mcexpanded/util/ModEventListeners.java
+++ src/main/java/net/nathan/mcexpanded/util/ModEventListeners.java
@@ -2,15 +2,14 @@ package net.nathan.mcexpanded.util;
 
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.nathan.mcexpanded.effect.BeeEffect;
 
 public class ModEventListeners {
     public static void registerEventListeners() {
         AttackEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
-            if (!world.isClient() && player instanceof PlayerEntity) {
-                BeeEffect.onEntityHit((PlayerEntity) player, (LivingEntity) entity);
+            if (!world.isClient() && entity instanceof LivingEntity) {
+                BeeEffect.onEntityHit(player, (LivingEntity) entity);
             }
             return ActionResult.PASS;
         });
